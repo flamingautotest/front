9137b81bfab6598bdb0bb3bdfb3e7ea9f3c2ed99
AymericM <aymeric.moehn@gmail.com>
2022-07-20 11:42:11 +0200

    feat(Button): add class option to Button

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,6 @@
 export default function Button(props) {
     return (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" {...props}>
+        <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ${props?.class}`} {...props}>
             {props.children}
         </button>
     )
