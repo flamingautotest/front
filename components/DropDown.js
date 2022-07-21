bf7e08476ed14b67f271a6b9777e7f28a39d5e7e
AymericM <aymeric.moehn@gmail.com>
2022-07-21 15:16:11 +0200

    fix(dropdown): fix width

--- components/DropDown.js
+++ components/DropDown.js
@@ -18,8 +18,8 @@ export default function DropDown(props) {
 	}
 
 	return (
-		<Menu as="div" className={["relative inline-block text-left", className].join(' ')}>
-			<Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+		<Menu as="div" className={["relative inline-block text-left w-56", className].join(' ')}>
+			<Menu.Button className="inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
 				{ activeItem.label }
 				<ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
 			</Menu.Button>
