f2cf1c53a782718f026ed9ecd58dabdac835adbf
AymericM <aymeric.moehn@gmail.com>
2022-07-21 15:07:05 +0200

    fix(dropdown): fix z-index

--- components/DropDown.js
+++ components/DropDown.js
@@ -33,7 +33,7 @@ export default function DropDown(props) {
 				leaveFrom="transform opacity-100 scale-100"
 				leaveTo="transform opacity-0 scale-95"
 			>
-				<Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none cursor-pointer">
+				<Menu.Items className="origin-top-right absolute z-50 right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none cursor-pointer">
 					{items.map((item, index) =>(
 						<Menu.Item key={index} onClick={() => switchActiveItem(item)}>
 							<div className={joinClassNames(activeItem.value === item.value ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm cursor-pointer')}>
