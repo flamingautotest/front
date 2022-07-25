d58870af711f567f5a4f7b9d026d8f557e209bba
AymericM <aymeric.moehn@gmail.com>
2022-07-25 15:11:31 +0200

    feat(components): add navbar

---
+++ components/Navbar.js
@@ -0,0 +1,61 @@
+import { useContext } from 'react'
+import { Button } from '~/components'
+import { UserContext } from '~/stores'
+import Link from 'next/link'
+
+export default function Navbar() {
+    const [userState, userDispatch] = useContext(UserContext)
+
+    const logout = () => {
+		userDispatch(user => {
+			user.isLoggedIn = false
+			user.email = false
+		})
+	}
+
+    return (
+        <div className='flex flex-row justify-between max-w-screen-lg w-full mx-auto items-center p-4'>
+            <Link href='/'>
+                <a>
+                    <h1 className='text-xl'>Flaming-Autotest</h1>
+                </a>
+            </Link>
+            <div className='flex flex-row items-center'>
+                {!userState.isLoggedIn ? (
+                    <div className='flex flex-row items-center'>
+                        <Link href="/login">
+                            <a>
+                                <Button
+                                    type={'secondary'}
+                                    size={'s'}
+                                    className={'ml-5'}
+                                >
+                                    Log in
+                                </Button>
+                            </a>
+                        </Link>
+                        <Link href="/register">
+                            <a>
+                                <Button
+                                    type={'primary'}
+                                    size={'s'}
+                                    className={'ml-5'}
+                                >
+                                    Register
+                                </Button>
+                            </a>
+                        </Link>
+                    </div>
+                ) : (
+                    <Button
+                        type={'secondary'}
+                        size={'s'}
+                        onClick={logout}
+                    >
+                        Log out
+                    </Button>
+                )}
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,78 +1,14 @@
-import { useContext, useState } from 'react'
-import Link from 'next/link'
-import { UserContext } from '~/stores'
 import { Button } from "~/components"
 
-function LoggedOutHome() {
-	return (
-		<>
-			<div className="m-5">
-				<Link href="/register">
-					<a>
-						<Button
-							type={'primary'}
-							rounded={true}
-							className={'mb-5'}
-						>
-							Register
-						</Button>
-					</a>
-				</Link>
-			</div>
-			<div className="m-5">
-				<Link href="/login">
-					<a>
-						<Button
-							type={'secondary'}
-							rounded={true}
-							className={'mb-5'}
-						>
-							Log in
-						</Button>
-					</a>
-				</Link>
-			</div>
-		</>
-	)
-}
-
-function LoggedInHome() {
-	const [userState, userDispatch] = useContext(UserContext)
-
-	const logout = () => {
-		userDispatch(user => {
-			user.isLoggedIn = false
-			user.email = false
-		})
-	}
-
-	return (
-		<div className='flex flex-col justify-start items-start'>
-			<p>Hello {userState.email} !</p>
-			<Button
-				type={'secondary'}
-				className={'mt-5'}
-				onClick={logout}
-			>
-				Log out
-			</Button>
-		</div> 
-	)
-}
-
 export default function Home() {
-	const [userState, userDispatch] = useContext(UserContext)
-
 	return (
-		<div className="rounded-xl m-10 flex flex-col items-start">
+		<div className="rounded-xl mt-10 flex flex-col items-start">
 			<h1 className="text-xl">Welcome to flaming' autotest</h1>
-			<div className="flex flex-row">
-				{userState.isLoggedIn ? (
-					<LoggedInHome />
-				): (
-					<LoggedOutHome />
-				)}
-			</div>
+			<Button
+				className={'mt-5'}
+			>
+				Open app
+			</Button>
 		</div>
 	)
 }
