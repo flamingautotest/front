b618e4904fc4429469197579daa5c5a1fb85d77b
AymericM <aymeric.moehn@gmail.com>
2022-07-27 11:34:57 +0200

    feat(index): add CreateTest conditional

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,13 @@ import { CreateTest, Landing, Upload } from '~/partials'
 export default function Home() {
 	const [userState] = useContext(UserContext)
 
-	if (!userState.isLoggedIn) return <Landing />
-	return <Upload />
+	if (!userState.isLoggedIn) {
+		return <Landing />
+	} else {
+		if (userState.fileUrl.length) {
+			return <CreateTest />
+		} else {
+			return <Upload />
+		}
+	}
 }
