477748181cc16430e6632ea826f480cf115aaef2
AymericM <aymeric.moehn@gmail.com>
2022-07-28 12:14:40 +0200

    fix(Upload): update styles

--- partials/Upload.js
+++ partials/Upload.js
@@ -11,17 +11,17 @@ export default function Upload() {
 
     return (
         <>
-            <p>Hello {userState.email} !</p>
             <form className='flex flex-col pt-28 items-center justify-center w-60 mx-auto'>
+                <h1 className='text-xl mb-10'>Hello {userState.firstName} {userState.lastName} !</h1>
                 <InputFile
-                    label={'Choose the test to upload'}
+                    label={'To begin, first upload your swagger file'}
                     name={'upload'}
                     required={true}
                     className={'w-full mt-5'}
                     onChange={(e) => setFile(e.target.files[0])}
                 />
                 <Button
-                    className={'mt-4'}
+                    className={'mt-8'}
                     type={'primary'}
                     size={'m'}
                     disabled={file.name ? false : true}
