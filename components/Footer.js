92c4894754f4927445775d09531b9d3b21c38ddf
aymxdd <aymxdd@gmail.com>
2022-10-05 17:12:43 +0200

    fix(footer): center text

--- components/Footer.js
+++ components/Footer.js
@@ -2,13 +2,10 @@ export default function Button(props) {
     const {
         className = '',
     } = props
+
     return (
-        <div className={[
-            'py-8 absolute',
-            className
-        ].join(' ')
-        }>
-            <span className={'text-center'}>Flaming-autotest © 2022</span>
+        <div className={['py-8 w-full mt-4 text-center', className].join(' ')}>
+            <span>Flaming-autotest © 2022</span>
         </div>
     )
 }
