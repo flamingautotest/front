523815a3b5c6958313203cc7a01c9f3878af10d8
ArmandBeni <armand.benichou@hetic.net>
2022-10-05 11:57:57 +0200

    fix(spinner): remove scroll when spinner

--- components/Spinner.js
+++ components/Spinner.js
@@ -2,7 +2,7 @@ import HeaderIllustration from '~/assets/header_illustration.png'
 
 export default function Spinner() {
     return (
-        <div className="h-screen flex flex-col justify-center items-center z-50">
+        <div className="absolute inset-0 h-screen flex flex-col justify-center items-center z-50">
             <img className='w-32 object-contain' src={HeaderIllustration.src} />
             <div className="loading-spinner">
                 <div></div>
