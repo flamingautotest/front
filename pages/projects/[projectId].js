64efac6f5b6c5f1c009b258247ef78a46b236431
carolyneFernandez <carolyne.nicolle@hotmail.com>
2022-10-05 16:18:11 +0200

    responsive page projects

--- pages/projects/[projectId].js
+++ pages/projects/[projectId].js
@@ -1,6 +1,7 @@
 import { Footer } from '~/components'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { joinClassNames } from '~/utils'
 
 export default function ProjectDetail() {
     const router = useRouter()
@@ -47,7 +48,7 @@ export default function ProjectDetail() {
 
     return (
         <div className='w-full mt-10'>
-            <div className='flex mb-16'>
+            <div className='flex mb-8 sm:mb-16'>
                 <div>
                     <Link href='/projects'>
                         <a className='text-blue-500'>
@@ -66,8 +67,8 @@ export default function ProjectDetail() {
                     />
                 </div>
             </div>
-            <table className='w-full m-0 mb-16'>
-                <thead className='h-16 border-gray-200 border-b text-gray-600'>
+            <table className='w-full m-0 mb-8 sm:mb-16'>
+                <thead className='h-16 border-gray-200 border-b text-gray-600 text-xs sm:text-base'>
                     <tr>
                     <th className='text-left'>Name</th>
                     <th className='text-left'>Creation date</th>
@@ -78,17 +79,27 @@ export default function ProjectDetail() {
                 <tbody>
                     {testSuites.map(testSuite => (
                         <Link key={testSuite.id} href={`/suite/${testSuite.id}`}>                
-                            <tr className='h-16 cursor-pointer border-gray-200 border-b text-gray-600' >
+                            <tr className='h-16 cursor-pointer border-gray-200 border-b text-gray-600 text-xs sm:text-base' >
                                 <td>{testSuite.name}</td>
                                 <td>{testSuite.creation_date}</td>
                                 <td>{testSuite.frequency}</td>
-                                <td className='flex items-center m-top mt-4'><p className='border-gray-200 border text-xs p-0.5 mr-1.5 rounded'>{testSuite.last_execution.status}</p> {testSuite.last_execution.date}</td>
+                                <td className='flex items-center m-top mt-4'>
+                                <p className={ 
+                                    joinClassNames(
+                                        'border-gray-200 border text-xs p-0.5 mr-1.5 rounded',
+                                        testSuite.last_execution.status == 'warning' ? 'text-red-900':'',
+                                        testSuite.last_execution.status == 'success' ? 'text-sky-900':'',
+                                        testSuite.last_execution.status == 'failure' ? 'text-amber-400':'',
+
+                                    )
+                                    }>{testSuite.last_execution.status}</p>
+                                     {testSuite.last_execution.date}</td>
                             </tr>
                         </Link>
                     ))}
                 </tbody>
             </table>
-            <div className='w-full flex justify-center text-gray-400'>
+            <div className='w-full flex justify-center text-gray-400 text-xs sm:text-sm'>
                 <p className='mr-12'>Rows per page:</p>
                 <p className='mr-12'>1-5 of 13</p>
                 <p className='text-2xl'>{'<   >'}</p>
