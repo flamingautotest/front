aa35f8f9df52b686d9ff338e9d19bdc7b44414ca
aymxdd <aymxdd@gmail.com>
2022-10-06 18:10:25 +0200

    fix(tools): remove useless next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
-	swcMinify: true,
-	webpack5: true,
-	webpack: (config) => {
-		config.resolve.fallback = {
-			fs: false,
-			path: false,
-		};
-		return config;
-	},
+	swcMinify: true
 }
 
 module.exports = nextConfig
