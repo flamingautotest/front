465f57873237f8a64b57e2a31d25be038e0ba1a2
aymxdd <aymxdd@gmail.com>
2022-10-13 15:45:18 +0200

    fix(Footer): fix spacing between text and logo

--- components/Footer.js
+++ components/Footer.js
@@ -7,9 +7,9 @@ export default function Button(props) {
     } = props
 
     return (
-        <div className={['flex flex-col w-full text-center absolute bottom-0 left-0', className].join(' ')}>
-            <span>Flaming-autotest © 2022</span>
+        <div className={['flex flex-col w-full text-center absolute mb-3 bottom-0 left-0', className].join(' ')}>
             <Image width={'40px'} height={'40px'} className='w-10 object-contain' src={HeaderIllustration} />
+            <span className='mt-3'>Flaming-autotest © 2022</span>
         </div>
     )
 }
