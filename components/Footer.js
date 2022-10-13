d4ccf19f3d596a4e5b2579f6dc3946de9c785504
Corentin Boulanouar <corentin.boulanouar@hetic.net>
2022-10-13 19:46:16 +0200

    fix(footer): Make Footer component click-through
    resolves a bug where Footer may prevent the user to click on "save" and "delete" test buttons

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Button(props) {
     } = props
 
     return (
-        <div className={['flex flex-col w-full text-center absolute mb-3 bottom-0 left-0', className].join(' ')}>
+        <div className={['flex flex-col w-full text-center absolute mb-3 bottom-0 left-0 pointer-events-none', className].join(' ')}>
             <Image width={'40px'} height={'40px'} className='w-10 object-contain' src={HeaderIllustration} />
             <span className='mt-3'>Flaming-autotest © 2022</span>
         </div>
